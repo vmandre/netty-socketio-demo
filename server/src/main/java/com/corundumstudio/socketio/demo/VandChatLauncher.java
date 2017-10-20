54014493b75b93949c6a6b86ff3ae395c342be7e
Vanderlei Matos Andre <vanderlei.andre.ext@tivit.com.br>
2017-10-20 00:26:58 -0300

    Alterações para testes

---
+++ server/src/main/java/com/corundumstudio/socketio/demo/VandChatLauncher.java
@@ -0,0 +1,39 @@
+package com.corundumstudio.socketio.demo;
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+
+public class VandChatLauncher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Configuration config = new Configuration();
+//        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        server.addEventListener("chatevent", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
+System.out.println("data => " + data);            	
+                server.getBroadcastOperations().sendEvent("chatevent", data);
+            }
+        });
+//        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+//        	@Override
+//        	public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
+//        		server.getBroadcastOperations().sendEvent("chatevent", data);
+//        	}
+//        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+
+}
